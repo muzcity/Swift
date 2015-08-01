721f0c4bd75a60b7ad4759c4f35b03c44ff5c1bb
brinicle <brinicle@brinicleui-MacBook-Pro.local>
2015-08-01 15:08:52 +0900

    uilabel에 attributeString 부분 샘플 추가.

--- SwiftSample/SwiftSample/UILabelSample/UILabelSampleViewController.swift
+++ SwiftSample/SwiftSample/UILabelSample/UILabelSampleViewController.swift
@@ -57,6 +57,35 @@ class UILabelSampleViewController: UIViewController {
 		self.view .addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[prevLabel][label(40)]", options: NSLayoutFormatOptions(0), metrics: nil, views: ["label" : label, "prevLabel":prevLabel]))
 		
 		
+		
+		//NSForegroundColorAttributeName : color
+		var attributedText = NSMutableAttributedString(string: "text attribute")
+		attributedText.addAttribute(NSForegroundColorAttributeName, value:UIColor.redColor(), range: NSMakeRange(0, 4));
+		
+		
+		prevLabel = label
+		label = UILabel()
+		
+		self.view.addSubview(label)
+		
+		label.backgroundColor = UIColor.yellowColor()
+		label.textAlignment = NSTextAlignment.Left
+		label.attributedText = attributedText
+		label.highlighted = false
+		label.highlightedTextColor = UIColor.whiteColor()
+		
+		label.setTranslatesAutoresizingMaskIntoConstraints(false)
+		
+		self.view .addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[label]|", options: NSLayoutFormatOptions(0), metrics: nil, views: ["label" : label]))
+		self.view .addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[prevLabel][label(40)]", options: NSLayoutFormatOptions(0), metrics: nil, views: ["label" : label, "prevLabel":prevLabel]))
+		
+		
+		
+		
+		
+		attributedText = NSMutableAttributedString(string: "text marquee")
+		attributedText.addAttribute(NSForegroundColorAttributeName, value:UIColor.redColor(), range: NSMakeRange(0, 4));
+		
     }
 
     override func didReceiveMemoryWarning() {
