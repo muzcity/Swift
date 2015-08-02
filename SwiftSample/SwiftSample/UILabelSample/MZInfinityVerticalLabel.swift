0a6eaed3e16ae8148003c4cf2a90dc26e7d04297
WONJUN KI <muzcity@WONJUNui-MacBook-Air.local>
2015-08-02 14:57:09 +0900

    /*! * 입력된 글자 높이에 맞게 label을 늘려주도록 autolayout설정  weak로 변수를 받아서 refCnt에 영향이 없게 처리함. * weakConstrantForHeight가 비었다면 autolayout설정을 알수 없으므로 frame 설정하도록 처리됨. * 기본 padding은 5 상하 10을 넣어줌. */

---
+++ SwiftSample/SwiftSample/UILabelSample/MZInfinityVerticalLabel.swift
@@ -0,0 +1,101 @@
+//
+//  MZInfinityVerticalLabel.swift
+//  SwiftSample
+//
+//  Created by WONJUN KI on 2015. 8. 2..
+//  Copyright (c) 2015년 muzcity. All rights reserved.
+//
+
+import UIKit
+
+/*!
+* 입력된 글자 높이에 맞게 label을 늘려주도록 autolayout설정  weak로 변수를 받아서 refCnt에 영향이 없게 처리함.
+* weakConstrantForHeight가 비었다면 autolayout설정을 알수 없으므로 frame 설정하도록 처리됨.
+*/
+
+class MZInfinityVerticalLabel: UILabel {
+    
+    var labelHeight : CGFloat = 0
+    var Padding : CGFloat = 5
+    
+    weak var weakConstraintForHeight : NSLayoutConstraint?
+    
+    convenience init(contentHeight height:CGFloat)
+    {
+        self.init(frame:CGRectZero)
+        
+        self.labelHeight = height
+    }
+    
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        self.labelHeight = 0
+        
+        self.numberOfLines = -1
+    }
+
+    private var myContent = 0
+    
+    override init(frame: CGRect) {
+        
+        super.init(frame: frame)
+        
+        self.labelHeight = frame.size.height
+        
+        self.numberOfLines = -1
+        
+        self.addObserver(self as NSObject, forKeyPath: "text", options: .New, context: nil)
+    }
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        
+        if keyPath == "text" {
+            
+            var sz : CGSize = self.frame.size
+            sz.height = 2000
+            
+            var thatSize = self.sizeThatFits(sz)
+            
+            sz.height = thatSize.height;
+            
+            self.labelHeight = sz.height + (Padding * 2) //top + bottom
+            
+            if weakConstraintForHeight != nil {
+                self.weakConstraintForHeight?.constant = self.labelHeight
+                
+                if self.superview != nil {
+                    self.superview?.updateConstraints()
+                    self.superview?.setNeedsUpdateConstraints()
+                }
+                
+            }
+            else {
+                self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, sz.width, sz.height)
+            }
+            
+            return ;
+        }
+        
+        super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
+    }
+    
+    
+    deinit{
+        println("MZInfinityVerticalLabel deinit")
+        
+        self.removeObserver(self as NSObject, forKeyPath: "text")
+    }
+    
+    
+
+}
--- SwiftSample/SwiftSample/UILabelSample/UILabelSampleViewController.swift
+++ SwiftSample/SwiftSample/UILabelSample/UILabelSampleViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class UILabelSampleViewController: UIViewController {
 
+    deinit {
+        println("UILabelSampleViewController deinit")
+    }
+    
 	override func loadView() {
 		super.loadView()
 		
@@ -86,6 +90,56 @@ class UILabelSampleViewController: UIViewController {
 		attributedText = NSMutableAttributedString(string: "text marquee")
 		attributedText.addAttribute(NSForegroundColorAttributeName, value:UIColor.redColor(), range: NSMakeRange(0, 4));
 		
+        
+        
+        
+        var infinityLabel : MZInfinityVerticalLabel = MZInfinityVerticalLabel()
+        self.view.addSubview(infinityLabel)
+        
+        prevLabel = label
+        
+        infinityLabel.backgroundColor = UIColor.greenColor()
+        
+        //for frame set.
+//        infinityLabel.frame = CGRectMake(0, 200, 375, 40)
+
+        //for autolayout set.
+        infinityLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.view .addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[label]|", options: NSLayoutFormatOptions(0), metrics: nil, views: ["label" : infinityLabel]))
+        self.view .addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[prevLabel][label]", options: NSLayoutFormatOptions(0), metrics: nil, views: ["label" : infinityLabel, "prevLabel":prevLabel]))
+        
+        var contraintForLabelHeight = NSLayoutConstraint.constraintsWithVisualFormat("V:[label(40)]", options: NSLayoutFormatOptions(0), metrics: nil, views: ["label":infinityLabel])
+        
+        self.view.addConstraint(contraintForLabelHeight[0] as! NSLayoutConstraint)
+        
+        //높이를 설정을 한 constraint를 넘겨줘서 설정하도록 한다.
+        infinityLabel.weakConstraintForHeight = contraintForLabelHeight[0] as? NSLayoutConstraint
+        
+        infinityLabel.text = "abcde"
+        
+        //일정시간이 지나서 사용자가 텍스트를 변경했다 친다.
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(2.0*Double(NSEC_PER_SEC))), dispatch_get_main_queue()) { () -> Void in
+            infinityLabel.text = "동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리\n화려강산 대한사람 대한으로 길이보전하세"
+        }
+        
+        
+        
+        
+        
+        
+        label = UILabel.new()
+        
+        self.view.addSubview(label)
+        
+        label.backgroundColor = UIColor.blueColor()
+        label.textAlignment = NSTextAlignment.Center
+        label.text = "center align"
+        
+        label.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
+        self.view .addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[label]|", options: NSLayoutFormatOptions(0), metrics: nil, views: ["label" : label]))
+        self.view .addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[infinityLabel][label(40)]", options: NSLayoutFormatOptions(0), metrics: nil, views: ["label" : label, "infinityLabel":infinityLabel]))
+        
     }
 
     override func didReceiveMemoryWarning() {
